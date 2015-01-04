3034decb1e5331fcdfc29b505f716c9a1608216d
Julie Pan <ycp217@nyu.edu>
2015-01-04 00:32:37 -0800

    implement app helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,32 +1,28 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
-  
-  def setup
-  	@base_title = "JC Language"
-  end
-  	
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "JC Language"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | #{@base_title}"
+    assert_select "title", "Help | JC Language"
   end
 
   test "should get about" do
   	get :about
   	assert_response :success
-    assert_select "title", "About | #{@base_title}"
+    assert_select "title", "About | JC Language"
   end
 
   test "should get contact" do
   	get :contact
   	assert_response :success
-    assert_select "title", "Contact | #{@base_title}"
+    assert_select "title", "Contact | JC Language"
   end
 end

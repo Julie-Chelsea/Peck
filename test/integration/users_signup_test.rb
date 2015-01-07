2332d9209fd1bc8e4bd1da54c39893620df4c5fe
Julie Pan <ycp217@nyu.edu>
2015-01-07 14:09:10 -0800

    Use SSl and Unicorn in production

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -4,7 +4,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
   	get signup_path
   	assert_no_difference 'User.count' do
-  		post users_path, user: { name: "", email: "user@invalid", username: "" password: "foo", password_confirmation: "bar" }
+  		post users_path, user: { name: "", email: "user@invalid", username: "", password: "foo", password_confirmation: "bar" }
   	end
   	assert_template 'users/new'
   end

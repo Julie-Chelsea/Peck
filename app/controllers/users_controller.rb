ef5559cb45d4d9808e2a6912d2a259063d2bbaa3
Julie Pan <ycp217@nyu.edu>
2015-01-06 20:30:55 -0800

    complete sign uo paths

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
-      # Handle a successful save.
+			flash[:success] = "Welcome to JC Lang!"
+			redirect_to @user
   		else
   			render 'new'
   		end
@@ -19,7 +20,7 @@ class UsersController < ApplicationController
 private
 
 	def user_params
-		params.require(:user).permit(:name, :email, :password,
+		params.require(:user).permit(:name, :email, :username, :password,
 			:password_confirmation)
 	end
 end

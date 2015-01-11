b65999b422d6cdf6591d211a83165f846858814c
Julie Pan <ycp217@nyu.edu>
2015-01-10 19:37:14 -0800

    add user index

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -6,6 +6,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
 	end
 
 	test "unsuccessful edit" do
+		log_in_as(@user)
 		get edit_user_path(@user)
 		assert_template 'users/edit'
 		patch user_path(@user), user: { name: "",
@@ -18,6 +19,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
 	end
 
 	test "successful edit" do
+		log_in_as(@user)
 		get edit_user_path(@user)
 		assert_template 'users/edit'
 		name = "Foo Bar"
@@ -36,4 +38,23 @@ class UsersEditTest < ActionDispatch::IntegrationTest
 		assert_equal @user.username, username
 	end
 
+	test "successful edit with friendly forwarding" do 
+		get edit_user_path(@user)
+		log_in_as(@user)
+		assert_redirected_to edit_user_path(@user)
+		name = "Foo Bar"
+		username = "foobar"
+		email = "foo@bar.com"
+		patch user_path(@user), user: { name: name,
+										email: email,
+										username: username,
+										password: "",
+										password_confirmation: "" }
+		assert_not flash.empty?
+		assert_redirected_to @user
+		@user.reload
+		assert_equal @user.name, name
+		assert_equal @user.email, email
+		assert_equal @user.username, username
+	end
 end

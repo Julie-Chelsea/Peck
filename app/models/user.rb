aa15f03b7a42cd707da461a2046c021381fe4fba
Julie Pan <ycp217@nyu.edu>
2015-01-12 21:04:06 -0800

    refactor activation controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,6 +41,17 @@ class User < ActiveRecord::Base
 		update_attribute(:remember_digest, nil)
 	end
 
+	# activates an account
+	def activate
+		update_attribute(:activated, true)
+		update_attribute(:activated_at, Time.zone.now)
+	end
+
+	# sends activation email
+	def send_activation_email
+		UserMailer.account_activation(self).deliver
+	end
+
 	private
 
 		# converts email to all lowercase

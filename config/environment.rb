3c616b5c83a32df0c7f88b8fbda30bfcc1956348
Julie Pan <ycp217@nyu.edu>
2015-01-13 20:51:56 -0800

    fix mailer

--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,6 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-host = 'pure-temple-5080.herokuapp.com'
-
 ActionMailer::Base.smtp_settings = {
 	:address          => 'smtp.sendgrid.net',
 	:port             => '587',

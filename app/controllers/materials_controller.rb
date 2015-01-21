c9d1e8580a6201954d30459a956ca6ec48eef353
Julie Pan <ycp217@nyu.edu>
2015-01-21 18:12:53 -0500

    add KOR list view

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -7,4 +7,8 @@ class MaterialsController < ApplicationController
 	def list
 		@materials = Material.all
 	end
+
+	def korean
+		@materials = Material.all
+	end
 end
--- app/models/material.rb
+++ app/models/material.rb
@@ -2,6 +2,5 @@ class Material < ActiveRecord::Base
 	self.abstract_class = true
 	establish_connection :development
 	self.table_name = "lessons_lesson"
-
 	
 end

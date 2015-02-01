a0f2b27bd77c0174290363a2ead4f5439a96a11e
Julie Pan <ycp217@nyu.edu>
2015-01-31 23:08:54 -0500

    new word model

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -1,11 +1,12 @@
 class MaterialsController < ApplicationController
 
 	def index
-		@material = Material.all
+		@materials = Material.all
 	end
 
 	def show
-		@material = Material.second
+		# is this how you are suppose to use this?? the find(params[:id]) stuff
+		@material = Material.find(params[:id])
 	end
 
 	def korean
--- app/models/material.rb
+++ app/models/material.rb
@@ -2,5 +2,4 @@ class Material < ActiveRecord::Base
 	self.abstract_class = true
 	establish_connection :development
 	self.table_name = "lessons_lesson"
-	
 end

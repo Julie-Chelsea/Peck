024630269e33012837560760e9b25219e0d56ab4
Julie Pan <ycp217@nyu.edu>
2015-02-13 20:58:42 -0500

    add link to connect to lessons/words

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -2,12 +2,12 @@ class MaterialsController < ApplicationController
 
 	def index
 		@materials = Material.all
-		@word = Word.all
 	end
 
 	def show
 		# is this how you are suppose to use this?? the find(params[:id]) stuff
 		@material = Material.find(params[:id])
+		@words = Word.all
 	end
 
 	def korean

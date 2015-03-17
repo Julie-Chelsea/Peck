3753cf5744db5a484184914661a0a036384ec20a
Chelesa Valentine <cv851@nyu.edu>
2015-03-17 02:22:11 -0400

    change format so folder is the app folder

--- db/migrate/20150201040843_create_words.rb
+++
@@ -1,8 +0,0 @@
-class CreateWords < ActiveRecord::Migration
-  def change
-    create_table :words do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,7 @@
-User.create!(name:  "Example User",
-             email: "exampleuser@railstutorial.org",
-             username: "exampleuser",
-             password:              "foobar123",
-             password_confirmation: "foobar123",
-             admin: true,
-             activated: true,
-             activated_at: Time.zone.now)
-
-99.times do |n|
-  name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
-  username = "example-#{n+1}"
-  password = "password"
-  User.create!(name:  name,
-               email: email,
-               username: username,
-               password:              password,
-               password_confirmation: password,
-               activated: true,
-               activated_at: Time.zone.now)
-end
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)

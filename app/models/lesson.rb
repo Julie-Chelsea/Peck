6ebeebf00a2f86e576e3ec03d1192ec971431960
Chelesa Valentine <cv851@nyu.edu>
2015-03-17 04:40:42 -0400

    fix start issues + finish header/home :hamster:

---
+++ app/models/lesson.rb
@@ -0,0 +1,9 @@
+class Lesson < ActiveRecord::Base
+	self.abstract_class = true
+	establish_connection :development
+	self.table_name = "lessons_lesson"
+
+	has_many :slides
+	has_many :words
+
+end
---
+++ app/models/slide.rb
@@ -0,0 +1,8 @@
+class Slide < ActiveRecord::Base
+	self.abstract_class = true
+	establish_connection :development
+	self.table_name = "lessons_slide"
+
+	belongs_to :lessons
+
+end
